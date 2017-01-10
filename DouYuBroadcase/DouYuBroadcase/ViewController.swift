46cfeaf8ff1cf12fa74b71afbe405136fe20070d
CongYuQing <cyq925872220@163.com>
2017-01-10 16:35:05 +0800

    create new application

---
+++ DouYuBroadcase/DouYuBroadcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouYuBroadcase
+//
+//  Created by 丛宇新 on 2017/1/10.
+//  Copyright © 2017年 Eric Cong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
